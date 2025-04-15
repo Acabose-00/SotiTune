3df04ba198af294c3818ebf503beda8849d82867
Acabose-00 <dieg.donosop@gmail.com>
2025-04-14 20:51:18 -0400

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'afinador',
+  webDir: 'www'
+};
+
+export default config;
