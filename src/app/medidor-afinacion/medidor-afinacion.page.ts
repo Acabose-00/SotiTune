9a6056ddbddc77b7200f0081a194ced8245f9ec7
Acabose-00 <dieg.donosop@gmail.com>
2025-05-03 22:47:07 -0400

    medidor afinacion <<version 1.0>>

--- src/app/medidor-afinacion/medidor-afinacion.page.ts
+++ src/app/medidor-afinacion/medidor-afinacion.page.ts
@@ -1,23 +1,25 @@
-import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar } from '@ionic/angular/standalone';
+import { Component, OnInit } from '@angular/core';
+import { IonicModule } from '@ionic/angular';
 import { TunerComponent } from '../components/tuner/tuner.component';
 import { ClavijeroComponent } from '../components/clavijero/clavijero.component';
-
+import { FooterComponent } from '../plantillas/footer/footer.component';
 
 @Component({
   selector: 'app-medidor-afinacion',
   templateUrl: './medidor-afinacion.page.html',
   styleUrls: ['./medidor-afinacion.page.scss'],
   standalone: true,
-  imports: [IonContent, IonHeader, IonTitle, IonToolbar, CommonModule, FormsModule, TunerComponent, ClavijeroComponent,]
+  imports: [CommonModule, FormsModule, IonicModule, TunerComponent, ClavijeroComponent, FooterComponent]
 })
 export class MedidorAfinacionPage implements OnInit {
 
+  selectedInstrument: string = 'bajo';
+  selectedStringIndex: number | null = null;
+  currentNote: string = '';
+
   constructor() { }
 
-  ngOnInit() {
-  }
-
+  ngOnInit() {}
 }
--- src/app/plantillas/footer/footer.component.ts
+++ src/app/plantillas/footer/footer.component.ts
@@ -1,14 +1,31 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { addIcons } from 'ionicons';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButton, IonButtons, IonIcon, IonMenuButton, IonLabel, IonSegment, IonSegmentButton, IonTab, IonTabBar, IonTabs, IonTabButton } from '@ionic/angular/standalone';
+import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star, speedometer, mic, list, musicalNoteOutline, flashOutline, trendingUpOutline, happyOutline, heartOutline, keyOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-footer',
   templateUrl: './footer.component.html',
   styleUrls: ['./footer.component.scss'],
+  imports: [IonTabButton, IonTab, IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, CommonModule, FormsModule,
+    IonButton, IonButton, IonButtons, IonIcon, IonMenuButton, IonLabel, IonSegment, IonSegmentButton, IonTabBar, IonTabs],
 })
 export class FooterComponent  implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { 
+  addIcons({musicalNoteOutline,flashOutline,trendingUpOutline,happyOutline,heartOutline,keyOutline,personCircle,list,create,ellipsisHorizontal,ellipsisVertical,helpCircle,search,star,speedometer,mic});
+}
 
   ngOnInit() {}
+  goToLogin() {
+    this.router.navigate(['/home']);
+  }
+
+  goToInstruments() {
+    this.router.navigate(['/menu-instrumentos']);
+  }
 
 }
