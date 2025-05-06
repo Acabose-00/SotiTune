1b7f3933ebf165b5bf1a5f872f219a2af8692591
LucasLete <luc.letelier@duocuc.cl>
2025-05-05 23:02:37 -0400

    iconos funcionales
    se le añadió funcionalidad a los íconos de los instrumentos, permitiendo navegar a la ventana de afinación por medio de los mismos (falta corregir que sean responsivos)

--- src/app/menu-instrumentos/menu-instrumentos.page.ts
+++ src/app/menu-instrumentos/menu-instrumentos.page.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
-import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButton, IonButtons, IonIcon, IonMenuButton, IonLabel, IonSegment, IonSegmentButton, IonTab, IonTabBar, IonTabs, IonTabButton } from '@ionic/angular/standalone';
+import { IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, IonButton, IonButtons, IonIcon, IonMenuButton, IonLabel, IonSegment, IonSegmentButton, IonTab, IonTabBar, IonTabs, IonTabButton, IonFab, IonFabButton } from '@ionic/angular/standalone';
 import { addIcons } from 'ionicons';
 import { create, ellipsisHorizontal, ellipsisVertical, helpCircle, personCircle, search, star, speedometer, mic, list, musicalNoteOutline, flashOutline, trendingUpOutline, happyOutline, heartOutline, keyOutline } from 'ionicons/icons';
 import { Router } from '@angular/router';
@@ -12,7 +12,7 @@ import { ClavijeroComponent } from "../components/clavijero/clavijero.component"
   templateUrl: './menu-instrumentos.page.html',
   styleUrls: ['./menu-instrumentos.page.scss'],
   standalone: true,
-  imports: [IonTabButton, IonTab, IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, CommonModule, FormsModule,
+  imports: [IonFabButton, IonFab, IonTabButton, IonTab, IonContent, IonHeader, IonTitle, IonToolbar, IonFooter, CommonModule, FormsModule,
     IonButton, IonButton, IonButtons, IonIcon, IonMenuButton, IonLabel, IonSegment, IonSegmentButton, IonTabBar, IonTabs, ClavijeroComponent]
 })
 export class MenuInstrumentosPage implements OnInit {
@@ -30,6 +30,10 @@ export class MenuInstrumentosPage implements OnInit {
 
   goToInstruments() {
     this.router.navigate(['/menu-instrumentos']);
+  }
+
+  goToAfinacion() {
+    this.router.navigate(['/medidor-afinacion']);
   }
 
 }
