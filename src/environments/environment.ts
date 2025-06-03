04143285c647af18208c64d196afc41f4d87638a
Acabose-00 <dieg.donosop@gmail.com>
2025-06-03 00:49:13 -0400

    Update environment.ts

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -5,7 +5,7 @@
 export const environment = {
   production: false,
   supabaseUrl: 'https://xxdicfgirafrdznvosxg.supabase.co/',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6Inh4ZGljZmdpcmFmcmR6bnZvc3hnIiwicm9sZSI6InNlcnZpY2Vfcm9sZSIsImlhdCI6MTc0ODkwMTcxMSwiZXhwIjoyMDY0NDc3NzExfQ.pEm_Gch2fDZBE9pR7Z9J8pTUMZlCmd4E-wTutiQ1hoA',
+  supabaseAnonKey: '',
 };
 
 /*
